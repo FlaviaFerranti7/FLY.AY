7b3b17fcfcec568b26e019ce11f3fefb563f3c23
leonardo.capozzi <leonardo.capozzi.1995@gmail.com>
2020-12-22 14:51:37 +0100

    chore: integrated Bundle class to pass params from MainActivity to EventsListFragment

--- app/src/main/java/com/flam/flyay/fragments/EventsListFragment.java
+++ app/src/main/java/com/flam/flyay/fragments/EventsListFragment.java
@@ -4,6 +4,7 @@ import android.content.Context;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.ListFragment;
 
 import android.util.Log;
@@ -31,11 +32,11 @@ import java.util.List;
 public class EventsListFragment extends ListFragment {
     private EventService service;
     private ConverterFromJsonToModel converterFromJsonToModel;
-    private List<String> events;
+    private List<Event> events;
     private OnEventsListListener onEventsListListener;
 
     public interface OnEventsListListener {
-        void onEventSelected(int index);
+        void onEventSelected(Event e);
     }
 
     public EventsListFragment() {}
@@ -54,11 +55,15 @@ public class EventsListFragment extends ListFragment {
     public void onCreate(Bundle savedInstanceState) {
         Log.d(".EventsListFragment", "create view...");
         super.onCreate(savedInstanceState);
+        Bundle arguments = getArguments();
 
         this.service = new EventService(this.getContext());
         this.converterFromJsonToModel = new ConverterFromJsonToModel();
         this.events = new ArrayList<>();
-        JSONObject params = getParams();
+        String currentDate = arguments.getString("currentDate");
+
+        JSONObject params = getParams(currentDate);
+        Log.d(".EventsListFragment", "parameters: [currentDate = '" + currentDate + "']");
 
         service.getEventsByDay(params, new ServerCallback() {
             @Override
@@ -75,20 +80,22 @@ public class EventsListFragment extends ListFragment {
                     eventsList.addView(btn);
 =======
             public void onSuccess(JSONObject result) {
+                List<String> eventsTitle = new ArrayList<>();
                 JSONArray containerResponse;
                 try {
                     containerResponse = result.getJSONArray("return");
 
                     for(int i = 0; i < containerResponse.length(); i ++) {
                         Event event = converterFromJsonToModel.converterFromJsonToEvent(containerResponse.getJSONObject(i));
-                        events.add(event.getTitle());
+                        events.add(event);
+                        eventsTitle.add(event.getTitle());
                     }
                 } catch (JSONException e) {
                     e.printStackTrace();
 >>>>>>> ccefb07... chore: integrated ListFragment
                 }
                 Log.d(".EventsListFragment",events.toString());
-                ArrayAdapter<String> adapter = new ArrayAdapter<>(getActivity(), android.R.layout.simple_list_item_1, events);
+                ArrayAdapter<String> adapter = new ArrayAdapter<>(getActivity(), android.R.layout.simple_list_item_1, eventsTitle);
                 setListAdapter(adapter);
             }
         });
@@ -97,7 +104,7 @@ public class EventsListFragment extends ListFragment {
     @Override
     public void onListItemClick(@NonNull ListView l, @NonNull View v, int position, long id) {
         super.onListItemClick(l, v, position, id);
-        onEventsListListener.onEventSelected(position);
+        onEventsListListener.onEventSelected(events.get(position));
     }
 
     @Override
@@ -105,10 +112,10 @@ public class EventsListFragment extends ListFragment {
         super.onSaveInstanceState(outState);
     }
 
-    private JSONObject getParams() {
+    private JSONObject getParams(String currentDay) {
         JSONObject params = new JSONObject();
         try {
-            params.put("currentDay", "30/01/2021");
+            params.put("currentDay", currentDay);
         } catch (JSONException e) {
             e.printStackTrace();
         }
