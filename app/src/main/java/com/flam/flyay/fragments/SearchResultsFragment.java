e62841fc3599b1f99ea9d8da6d7abc4299f19dd8
antonella.angrisani <antonella.angrisani97@gmail.com>
2021-01-14 18:18:22 +0100

    fix: search by place

--- app/src/main/java/com/flam/flyay/fragments/SearchResultsFragment.java
+++ app/src/main/java/com/flam/flyay/fragments/SearchResultsFragment.java
@@ -12,11 +12,18 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.annotation.RequiresApi;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.DividerItemDecoration;
+import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.flam.flyay.R;
 import com.flam.flyay.adapter.EventAdapter;
 import com.flam.flyay.model.Event;
+import com.flam.flyay.model.subevent.FestivityEvent;
+import com.flam.flyay.model.subevent.FinancesEvent;
+import com.flam.flyay.model.subevent.FreeTimeEvent;
+import com.flam.flyay.model.subevent.StudyEvent;
+import com.flam.flyay.model.subevent.WellnessEvent;
 import com.flam.flyay.services.EventService;
 import com.flam.flyay.services.ServerCallback;
 import com.flam.flyay.util.ConverterFromJsonToModel;
@@ -76,22 +83,36 @@ public class SearchResultsFragment extends Fragment {
                         Log.d(".SearchResultsFragment",  " eventsFiltered: " + eventsFiltered.toString());
                         eventsFiltered.add(e);
                     }
-                    if (!Utils.isEmptyOrBlank(searchPlace) && e.getTitle().toLowerCase().contains(searchPlace)) {
-                        Log.d(".SearchResultsFragment", e.toString());
-                        Log.d(".SearchResultsFragment",  " eventsFiltered: " + eventsFiltered.toString());
-                        eventsFiltered.add(e);
-                    }
                     if (checkedCategory.contains(e.getCategory())){
                         Log.d(".SearchResultsFragment", e.toString());
                         Log.d(".SearchResultsFragment",  " eventsFilteredPerCategories: " + eventsFiltered.toString());
                         eventsFiltered.add(e);
                     }
-
+                    if(e.getClass() == FreeTimeEvent.class && !Utils.isEmptyOrBlank(searchPlace) && ((FreeTimeEvent) e).getPlace().toLowerCase().contains(searchPlace)){
+                        Log.d(".SearchResultsFragment", e.toString());
+                        Log.d(".SearchResultsFragment",  " eventsFiltered: " + eventsFiltered.toString());
+                        eventsFiltered.add(e);
+                    }
+                    else if(e.getClass() == WellnessEvent.class && !Utils.isEmptyOrBlank(searchPlace) && ((WellnessEvent) e).getPlace().toLowerCase().contains(searchPlace)){
+                        Log.d(".SearchResultsFragment", e.toString());
+                        Log.d(".SearchResultsFragment",  " eventsFiltered: " + eventsFiltered.toString());
+                        eventsFiltered.add(e);
+                    }
+                    else if(e.getClass() == StudyEvent.class && !Utils.isEmptyOrBlank(searchPlace) && ((StudyEvent) e).getPlace().toLowerCase().contains(searchPlace)){
+                        Log.d(".SearchResultsFragment", e.toString());
+                        Log.d(".SearchResultsFragment",  " eventsFiltered: " + eventsFiltered.toString());
+                        eventsFiltered.add(e);
+                    }
                 }
 
                 Log.d(".SearchResultsFragment",  " eventsFiltered: " + eventsFiltered.toString());
                 EventAdapter filteredEventAdapter = new EventAdapter(eventsFiltered, null);
+                LinearLayoutManager layoutManager = new LinearLayoutManager(getActivity());
                 listRecyclerView.setAdapter(filteredEventAdapter);
+                listRecyclerView.setLayoutManager(layoutManager);
+                DividerItemDecoration dividerItemDecoration = new DividerItemDecoration(listRecyclerView.getContext(),
+                        layoutManager.getOrientation());
+                listRecyclerView.addItemDecoration(dividerItemDecoration);
                 filteredEventAdapter.notifyDataSetChanged();
             }
         });
