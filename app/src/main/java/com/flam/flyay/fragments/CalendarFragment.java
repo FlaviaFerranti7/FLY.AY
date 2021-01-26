3905bb12eb61f5ee9147adb7cc34d56dfba2e620
MichelaCap <capece.1702284@studenti.uniroma1.it>
2021-01-26 20:51:31 +0100

    chore: show events under calendar

--- app/src/main/java/com/flam/flyay/fragments/CalendarFragment.java
+++ app/src/main/java/com/flam/flyay/fragments/CalendarFragment.java
@@ -2,6 +2,7 @@ package com.flam.flyay.fragments;
 
 import android.app.DatePickerDialog;
 import android.app.Dialog;
+import android.content.Context;
 import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 import android.util.Log;
@@ -20,14 +21,26 @@ import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatDelegate;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
+import androidx.recyclerview.widget.DividerItemDecoration;
+import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.flam.flyay.MainActivity;
 import com.flam.flyay.R;
 import com.flam.flyay.SearchActivity;
+import com.flam.flyay.adapter.EventAdapter;
+import com.flam.flyay.model.Event;
+import com.flam.flyay.services.EventService;
+import com.flam.flyay.services.ServerCallback;
+import com.flam.flyay.util.ConverterFromJsonToModel;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Calendar;
+import java.util.List;
 import java.util.Locale;
 import java.util.TimeZone;
 
@@ -38,6 +51,10 @@ public class CalendarFragment extends Fragment {
     private String selectedDate;
     private Calendar c;
     private SimpleDateFormat df;
+
+    private EventService service;
+    private HomeFragment.OnEventsListListener onEventsListListener;
+    private List<Event> events;
 
     public CalendarFragment() {
         // Required empty public constructor
@@ -53,6 +70,12 @@ public class CalendarFragment extends Fragment {
         View view = inflater.inflate(R.layout.calendar_fragment, container, false);
         linearLayout = view.findViewById(R.id.fragment_calendar);
 
+        final RecyclerView listRecyclerView = view.findViewById(R.id.events_recycler);
+        listRecyclerView.setNestedScrollingEnabled(false);
+
+        this.service = new EventService(this.getContext());
+        this.events = new ArrayList<>();
+
         Bundle arguments = getArguments();
         selectedDate = arguments.getString("currentDate");
 
@@ -61,6 +84,27 @@ public class CalendarFragment extends Fragment {
         ((MainActivity) getActivity()).getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
         addCalendar();
+
+        JSONObject params = getParams(selectedDate);
+        Log.d(".EventsListFragment", "parameters: [currentDate = '" + selectedDate + "']");
+
+        service.getEventsByDay(params, new ServerCallback() {
+            @Override
+            public void onSuccess(Object result) {
+                events = (List<Event>) result;
+                Log.d(".EventsListFragment", events.toString());
+
+                EventAdapter eventAdapter = new EventAdapter(events, onEventsListListener);
+                LinearLayoutManager layoutManager = new LinearLayoutManager(getActivity());
+                listRecyclerView.setAdapter(eventAdapter);
+                listRecyclerView.setLayoutManager(layoutManager);
+                DividerItemDecoration dividerItemDecoration = new DividerItemDecoration(listRecyclerView.getContext(),
+                        layoutManager.getOrientation());
+                listRecyclerView.addItemDecoration(dividerItemDecoration);
+                eventAdapter.notifyDataSetChanged();
+            }
+        });
+
 
         return view;
     }
@@ -97,6 +141,17 @@ public class CalendarFragment extends Fragment {
             menu.getItem(i).setVisible(false);
         }
         super.onCreateOptionsMenu(menu, inflator);
+    }
+
+    private JSONObject getParams(String currentDay) {
+        JSONObject params = new JSONObject();
+        try {
+            params.put("currentDay", currentDay);
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
+        return params;
     }
 
 }
